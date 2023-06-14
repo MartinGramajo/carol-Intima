3f16cd3f38d30e24514c3ec595a0cc3ba241775e
¨mago¨ <martingramajo08@gmail.com>
2023-06-14 16:35:29 -0300

    agrego carousel cel fotos cuadradas

--- src/components/productosDestacados/ProductosDestacados.jsx
+++ src/components/productosDestacados/ProductosDestacados.jsx
@@ -1,5 +1,8 @@
 import { Image } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
+import foto1 from "../../assets/img/1.jpg";
+import foto2 from "../../assets/img/2.jpg";
+import foto3 from "../../assets/img/3.jpg";
 
 export default function ProductosDestacados() {
   return (
@@ -12,12 +15,7 @@ export default function ProductosDestacados() {
             alt="banner"
             fluid
           />
-          <Image
-            className="d-sm-none d-block"
-            src="https://www.casayonimayorista.com.ar/image/cache/catalog/slider-home-0-1903x800.jpg"
-            alt="banner"
-            fluid
-          />
+          <Image className="d-sm-none d-block" src={foto1} alt="banner" fluid />
         </Carousel.Item>
         <Carousel.Item className="d-flex align-items-center justify-content-center">
           <Image
@@ -26,12 +24,7 @@ export default function ProductosDestacados() {
             alt="banner"
             fluid
           />
-          <Image
-            className="d-sm-none d-block"
-            src="https://www.casayonimayorista.com.ar/image/cache/catalog/slider-home-2-1903x800.jpg"
-            alt="banner"
-            fluid
-          />
+          <Image className="d-sm-none d-block" src={foto2} alt="banner" fluid />
         </Carousel.Item>
         <Carousel.Item className="d-flex align-items-center justify-content-center">
           <Image
@@ -40,12 +33,7 @@ export default function ProductosDestacados() {
             alt="banner"
             fluid
           />
-          <Image
-            className="d-sm-none d-block"
-            src="https://www.casayonimayorista.com.ar/image/cache/catalog/slider-home-1-1903x800.jpg"
-            alt="banner"
-            fluid
-          />
+          <Image className="d-sm-none d-block" src={foto3} alt="banner" fluid />
         </Carousel.Item>
       </Carousel>
     </>
